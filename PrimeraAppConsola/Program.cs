d4eb089c11513b282af49a2e2166a7e14eb7c77b
ICoelloC <51157875+ICoelloC@users.noreply.github.com>
2020-09-15 10:18:01 +0200

    Update Program.cs
    Modificamos de forma externa

--- PrimeraAppConsola/Program.cs
+++ PrimeraAppConsola/Program.cs
@@ -8,6 +8,8 @@ namespace PrimeraAppConsola
         {
             //actualización del mensaje al castellano
             Console.WriteLine("Hola Mundo! DAM");
+            //modificamos desde el enetorno web de GitHub
+            Console.WriteLine("Adiós");
         }
     }
 }
